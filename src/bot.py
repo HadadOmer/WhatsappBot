bf254a8b68ca042ae6b4a2201caee8abebb54daf
OmerHadad <omerhadad10@gmail.com>
2025-03-19 01:55:49 +0200

    Basic code for green api integration

--- src/bot.py
+++ src/bot.py
@@ -1,33 +1,35 @@
-from yowsup.env import YowsupEnv
-from yowsup.stacks import YowsupStackBuilder
-from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
+from typing import Final, List, Dict
+from whatsapp_api_client_python import API
+import os
+from dotenv import load_dotenv
 
-class PollBot:
-    def __init__(self, credentials):
-        stack_builder = YowsupStackBuilder()
-        self.stack = stack_builder.pushDefaultLayers(True).build()
-        self.stack.setCredentials(credentials)
+load_dotenv('.env')
+INSTANCE_ID: Final[str] = os.getenv('INSTANCE_ID')
+API_TOKEN_INSTANCE: Final[str] = os.getenv('API_TOKEN_INSTANCE')
+PHONE_NUMBER : Final[str] = os.getenv('PHONE_NUMBER')
 
-    def start(self):
-        print("📢 Poll Bot is now running...")
-        self.stack.loop()
+greenAPI: API.GreenAPI = API.GreenAPI(INSTANCE_ID, API_TOKEN_INSTANCE)
 
-    def on_message(self, message):
-        if message.getBody().lower() == "!poll":
-            self.send_message(message.getFrom(), "📊 *New Poll:* \n1️⃣ Yes \n2️⃣ No \n\nReply with 1 or 2.")
+OPTIONS = [
+    {"optionName": "Red"},
+    {"optionName": "Green"},
+    {"optionName": "Blue"}
+]
+QUESTION = "Please choose a color:"
 
-    def send_message(self, recipient, text):
-        msg = TextMessageProtocolEntity(text, to=recipient)
-        self.stack.send(msg)
+def send_pole(question: str, options: List[Dict[str, str]], phone_number: str):
+    response = greenAPI.sending.sendPoll(
+    f"{phone_number}@c.us",
+    question,
+    options,
+    multipleAnswers=True
+    )
+    print(response.data)
 
-if __name__ == "__main__":
-    import os
-    PHONE = os.getenv("PHONE")
-    PASSWORD = os.getenv("PASSWORD")
 
-    if not PHONE or not PASSWORD:
-        print("❌ Error: PHONE and PASSWORD environment variables are required!")
-        exit(1)
+def main() -> None:
+    send_pole(QUESTION, OPTIONS, PHONE_NUMBER)
 
-    bot = PollBot((PHONE, PASSWORD))
-    bot.start()
+
+if __name__ == '__main__':
+    main()
